d1b4a0239de4dfd732f2a51c0f53635b66b29ca5
VetheonGames <ceo@pixelridgesoftworks.com>
2025-10-05 22:50:33 -0600

    remove set -e from deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,8 @@
 # Source-License Unified Deploy Script
 # Simplified deployment script for Unix systems (Linux/macOS)
 
-set -e
+# Note: Removed 'set -e' to prevent script from terminating parent shell when sourced
+# Instead, we'll handle errors explicitly in each function
 
 # Default values
 ACTION="help"
